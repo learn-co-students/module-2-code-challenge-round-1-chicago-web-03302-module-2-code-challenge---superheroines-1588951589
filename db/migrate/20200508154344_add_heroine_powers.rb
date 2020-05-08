15479ca15cfb66d7d02af1a81849b7ed1d40ee06
Sam Escobar <sescobar8714@gmail.com>
2020-05-08 10:13:13 -0700

    finished

---
+++ db/migrate/20200508154344_add_heroine_powers.rb
@@ -0,0 +1,10 @@
+class AddHeroinePowers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :heroine_powers do |t|
+      t.integer :heroine_id
+      t.integer :power_id
+
+      t.timestamps 
+    end
+  end
+end
