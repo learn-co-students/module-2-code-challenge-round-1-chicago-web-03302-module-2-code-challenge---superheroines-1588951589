a6979a991f982e99205923748bf286010511fcb3
imperfectcastle <bart.sakowski@gmail.com>
2020-05-08 12:12:46 -0500

    code challenge submit

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,27 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find_by(params[:id])
+  end
+
+  def edit
+    @power = Power.find_by(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id]) #i think i figured it out, but right as time was called, this is incomplete
+    heroine = Heroine.find_by(params[:power][:heroine_id])
+    @power.heroine << heroine #creates a join, but throws an error, not sure why.
+    redirect_to power_path(@power)
+  end
+
+
+  private
+
+
+  def power_params
+    params.require(:power).permit(:heroine_ids)
+  end
 end
