140c1c29b11a5533b86278ecb90198ec698cbf98
jayastronomic <juliansmith94@hotmail.com>
2020-05-08 12:17:06 -0500

    complet

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,33 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new 
+    @heroine = Heroine.new 
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(name: params[:name], super_name: params[:super_name])
+    @heroine_power = HeroinePower.create(heroine_id: @heroine.id, power_id: params[:power_id])
+    redirect_to heroine_path(@heroine)
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def update
+      @heroine = Heroine.update(name: params[:name], super_name: params[:super_name])
+      @heroine_power = HeroinePower.update(heroine_id: @heroine.id, power_id: params[:power_id])
+      redirect_to heroine_path(@heroine)
+  end
+
+  end
+
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,12 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroine_powers
+    validates :name, presence: true
+    validates :super_name, presence: true
+
+    private
+
+    def 
+    end
+
 end
